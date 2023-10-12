b16d1e6b5ffb2162c42b3bda2dfe1d3c35a3c8b1
bulat <bulatxajrutdinoff@yandex.ru>
2023-10-12 12:23:15 +0300

    removing async cause SQLAlchemy doesn't have compatibility for using await directly

--- todo_app/api/user_handlers.py
+++ todo_app/api/user_handlers.py
@@ -16,7 +16,7 @@ user_router = APIRouter()
 
 
 @user_router.post("/", response_model=schemas.User)
-async def create_user(
+def create_user(
     user: schemas.UserCreate, db: Session = Depends(get_db)
 ) -> schemas.User:
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -26,15 +26,15 @@ async def create_user(
 
 
 @user_router.get("/", response_model=list[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_users(db, skip=skip, limit=limit)
 
 
 @user_router.get("/{user_id}", response_model=schemas.User)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db)):
     return crud.get_user(db, user_id)
 
 
 @user_router.post("/{user_id}/todos", response_model=schemas.Todo)
-async def create_todo_for_user(user_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+def create_todo_for_user(user_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)):
     return crud.create_user_todo(db, user_id=user_id, todo=todo)
