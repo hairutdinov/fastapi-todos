4235a8c1538e4afedc047f1109b0430445758311
bulat <bulatxajrutdinoff@yandex.ru>
2023-10-12 20:09:01 +0300

    settings file which reads .env variables and create vars for app

---
+++ app/settings.py
@@ -0,0 +1,20 @@
+from envparse import Env
+from pathlib import Path
+
+
+ROOT_DIR = Path(".").absolute()
+APP_DIR = ROOT_DIR / 'app'
+
+env = Env()
+env.read_envfile(ROOT_DIR / ".env")
+
+DB_HOST = env.str("DB_HOST", default="localhost")
+DB_NAME = env.str("DB_NAME", default="todo")
+DB_USERNAME = env.str("DB_USERNAME", default="postgres")
+DB_PASSWORD = env.str("DB_PASSWORD", default="postgres")
+DB_PORT = env.str("DB_PORT", default="5432")
+
+DATABASE_URL = env.str(
+    "DB_URL",
+    default=f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+)
